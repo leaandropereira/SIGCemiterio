8a70cf082229f18dfeb8ac888b0d6acbad769221
Leandro Pereira <83283788+leaandropereira@users.noreply.github.com>
2023-08-05 13:12:10 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,33 @@
 // No seu arquivo JavaScript
 const installButton = document.getElementById('install-button');
+const dismissButton = document.getElementById('dismiss-button');
+const installPopup = document.getElementById('install-popup');
+
+let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', event => {
   event.preventDefault();
-  const deferredPrompt = event;
-  
-  installButton.addEventListener('click', () => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then(choiceResult => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('O usuário aceitou a instalação do PWA');
-      } else {
-        console.log('O usuário recusou a instalação do PWA');
-      }
-      deferredPrompt = null;
-    });
+  deferredPrompt = event;
+
+  // Exibe o pop-up para lembrar o usuário de instalar o PWA
+  installPopup.style.display = 'block';
+});
+
+// Evento para o botão "Instalar" no pop-up
+installButton.addEventListener('click', () => {
+  installPopup.style.display = 'none';
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then(choiceResult => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('O usuário aceitou a instalação do PWA');
+    } else {
+      console.log('O usuário recusou a instalação do PWA');
+    }
+    deferredPrompt = null;
   });
+});
+
+// Evento para o botão "Fechar" no pop-up
+dismissButton.addEventListener('click', () => {
+  installPopup.style.display = 'none';
 });
